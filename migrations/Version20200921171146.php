67e089b1a6cfc5b5800e8888027e58de0f63cf7c
Fenitriniaina <git.fandrianarisata@gmail.com>
2020-09-22 18:23:46 +0200

    mepita

--- migrations/Version20200921171146.php
+++ migrations/Version20200921171146.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200921082353 extends AbstractMigration
+final class Version20200921171146 extends AbstractMigration
 {
     public function getDescription() : string
     {
